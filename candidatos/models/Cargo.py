aed72c86d9f55c2e7e80071402f24ac1e9f5f7d2
AndersonBatalha <andersonpbatalha@gmail.com>
2018-11-19 23:27:48 -0200

    Mudança da versão do Django

--- candidatos/models/Cargo.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Cargo(models.Model):
-    desc_cargo = models.CharField(max_length=20)
-
-    class Meta:
-        abstract = 'Cargo pretendido pelo candidato'
-        verbose_name_plural = 'Cargos'
--- candidatos/models/Municipio.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Municipio(models.Model):
-    nome_municipio = models.CharField(max_length=75)
-
-    class Meta:
-        verbose_name_plural = 'Municípios'
