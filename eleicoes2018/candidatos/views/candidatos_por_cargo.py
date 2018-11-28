ed1ab8a6163b627d9bae9fa0c5bc1506a1f8edef
AndersonBatalha <andersonpbatalha@gmail.com>
2018-11-27 23:24:58 -0200

    Criada view e template para exibir os candidatos por cargo

--- eleicoes2018/candidatos/views/candidatos_por_cargo.py
+++ eleicoes2018/candidatos/views/candidatos_por_cargo.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 
-from candidatos.models import Cargo, Candidato
+from ..models import Cargo, Candidato
 
 '''
 Descriçao: com base no cargo escolhido pelo usuario (governador ou presidente), o sistema ira retornar uma lista com os candidatos 
@@ -11,15 +11,20 @@ class CandidatosCargo(View):
     context_dict = {}
     template = 'candidatos/candidatos_cargo.html'
 
-    def get(self, request, c):
+    def get(self, request, cargo=None):
         try:
-            cargo = Cargo.objects.get(desc_cargo__icontains=c)
-            self.context_dict['cargo'] = cargo
-
-            candidatos = Candidato.objects.filter(cargo = cargo)
+            self.cargo = Cargo.objects.filter(desc_cargo__icontains=cargo)
+            print(self.cargo)
+            self.candidatos = Candidato.objects.filter(cargo = self.cargo).order_by('partido', 'cargo')
         except Cargo.DoesNotExist:
+            self.context_dict['cargo'] = None
             self.context_dict['candidatos'] = None
+
+        self.context_dict['cargo'] = self.cargo
+        self.context_dict['candidatos'] = self.candidatos
+        self.context_dict['qtd_resultados'] = len(self.candidatos)
+
 
         print(self.context_dict)
 
-        return render(request, self.template, context=self.context_dict)
+        return render(request, self.template, self.context_dict)
