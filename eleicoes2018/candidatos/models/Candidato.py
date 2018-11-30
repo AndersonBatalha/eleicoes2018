38cf269380619573e2c702c45146541d168e5be7
AndersonBatalha <andersonpbatalha@gmail.com>
2018-11-30 00:57:55 -0200

    Adicionado campo 'slug' ao model 'Candidato' (nao funcionou)

--- eleicoes2018/candidatos/models/Candidato.py
+++ eleicoes2018/candidatos/models/Candidato.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 from .Candidatura import Candidatura
 from .Cargo import Cargo
@@ -19,6 +20,7 @@ class Candidato(models.Model):
     nacionalidade = models.CharField(max_length=50, help_text="Nacionalidade")
     data_nasc = models.DateField(help_text="Data de nascimento")
     numero_candidato = models.IntegerField(help_text="Número do candidato")
+    slug_nome = models.SlugField(blank=True)
 
     cargo = models.ForeignKey(Cargo)
     partido = models.ForeignKey(Partido)
@@ -28,6 +30,10 @@ class Candidato(models.Model):
 
     class Meta:
         verbose_name_plural = "Candidatos"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.nome_urna)
+        super(Candidato,self).save(*args, **kwargs)
 
     def __str__(self):
         return self.nome_urna
--- eleicoes2018/candidatos/models/Turno_Eleicao.py
+++ eleicoes2018/candidatos/models/Turno_Eleicao.py
@@ -1,12 +1,14 @@
 from django.db import models
 
+from ..models import Eleicao
 
 class Turno_Eleicao(models.Model):
     data_eleicao = models.DateField(help_text='Data da eleição')
     turno = models.IntegerField(help_text='1º ou 2º turno')
+    eleicao = models.ForeignKey(Eleicao)
 
     class Meta:
         verbose_name_plural = 'Turnos da Eleição'
 
     def __str__(self):
-        return "%dº turno - Dia %s/%s/%s" %(self.turno, str(self.data_eleicao.day), str(self.data_eleicao.month), str(self.data_eleicao.year))
+        return "%dº turno (%s/%s/%s)" %(self.turno, str(self.data_eleicao.day), str(self.data_eleicao.month), str(self.data_eleicao.year))
