589f33798d22b22b2e1aafcc24afdce4a9ea3d65
AndersonBatalha <andersonpbatalha@gmail.com>
2019-02-14 14:51:01 -0200

    Alterada versão do Django

--- eleicoes2018/candidatos/views/detalhes_candidato.py
+++ eleicoes2018/candidatos/views/detalhes_candidato.py
@@ -25,13 +25,14 @@ class DetalhesCandidato(View):
             self.cargo = Cargo.objects.get(desc_cargo=self.candidato.cargo.desc_cargo)
             self.municipio = Municipio.objects.get(nome_municipio=self.candidato.municipio.nome_municipio)
             self.candidatura = Candidatura.objects.get(pk=self.candidato.candidatura_id)
-
         except Candidato.DoesNotExist:
             self.candidato = None
+            print ('candidato não existe')
         except Coligacao.DoesNotExist:
             self.coligacao = None
+            print ('coligação não existe')
+            print(self.candidato, self.candidato.local_eleicao, self.candidato.partido, self.coligacao)
 
-        print (self.local_eleicao)
         self.context_dict['candidato'] = self.candidato
         self.context_dict['cargo'] = self.cargo
         self.context_dict['partido'] = self.partido
@@ -39,7 +40,13 @@ class DetalhesCandidato(View):
         self.context_dict['candidatura'] = self.candidatura
         self.context_dict['local'] = self.local_eleicao
         self.context_dict['coligacao'] = self.coligacao
-        if self.partidos_coligacao != None:
-            self.context_dict['partidos'] = self.partidos_coligacao[0].partido.all()
+
+        if self.partidos_coligacao:
+            if len(self.partidos_coligacao) == 0:
+                self.context_dict['partidos'] = None
+            elif len(self.partidos_coligacao) > 1:
+                    self.context_dict['partidos'] = self.partidos_coligacao[0].partido.all()
+            else:
+                self.context_dict['partidos'] = self.partidos_coligacao.partido.all()
 
         return render(request, self.template, self.context_dict)
